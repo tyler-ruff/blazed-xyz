b1f2ab33d61cc800a04f0f8c2ea7a053778b4a08
tyler-ruff <hello@blazed.space>
2024-11-07 23:11:14 -0500

    Style upgrades
    Header Programmatic render & style upgrades

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,26 @@
 
 export const SITE_TITLE = 'Blazed Publishing';
 export const SITE_DESCRIPTION = 'Blazed Publishing is the publishing business division of Blazed Labs.';
+export const MAIN_MENU = [
+    {
+        title: "Home",
+        path: "/"
+    },
+    {
+        title: "About",
+        path: "/about"
+    },
+    {
+        title: "Blog",
+        path: "#",
+        children: [
+            { title: "Hello 1", path: "/" },
+            { title: "Hello 2", path: "/" },
+            { title: "Hello 3", path: "/" },
+        ]
+    },
+    {
+        title: "Contact",
+        path: "https://blazedlabs.com/contact"
+    },
+];
