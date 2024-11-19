c9b654566e6e4e1d18819c35a437f90a1624a9e2
tyler-ruff <hello@blazed.space>
2024-11-19 01:20:31 -0500

    0.1.3
    Dark mode, dynamically rendered manifest.json file, dynamically rendered robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,12 @@ import netlify from '@astrojs/netlify';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blazed.xyz',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), react()],
   output: 'server',
   adapter: netlify(),
 });
